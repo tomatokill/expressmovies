80ac586ba52d30fa99eb24e12b826a81cf30c09e
Richard Cop <richard.cop@gmail.com>
2019-01-30 23:37:14 +0100

    post form with fetch et multer

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser');
+const multer=require('multer');
+const upload = multer();
+
+let frenchMovies = [];
 
 app.use('/public',express.static('public'));
 
@@ -13,20 +17,36 @@ app.get('/movies',function(req,res)
 {
     const title = "Films français des 30dernières années";
 
-    const french_movies = [
+    frenchMovies = [
         {title: "Le fabuleux destin d'Amélie Poulain", year: 2001},
         {title: "Buffet fro", year: 1979},
         {title: "Le diner de cons", year: 1998},
         {title: "sdf sdfsdf", year: 2012}
     ]
-    res.render('movies', {titleMovie:title, frenchMovies: french_movies});
+    res.render('movies', {titleMovie:title, frenchMovies: frenchMovies});
 })
 
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 
-app.post('/movies', urlencodedParser,(req,res) =>{
-    console.log(req.body);
-    res.sendStatus(201);
+// app.post('/movies', urlencodedParser,(req,res) =>{
+//     console.log(req.body);
+//     console.log("movie title",req.body.movieTitle);
+//     const newMovie = {title: req.body.movieTitle, year: parseInt(req.body.movieYear)};
+//     frenchMovies = [...frenchMovies,newMovie];
+//     console.log(frenchMovies);
+//     res.sendStatus(201);
+// })
+
+app.post('/movies', upload.fields([]), (req,res)=>{
+    if (!req.body) {
+        return res.status(500);
+    } else {
+        const formData = req.body;
+        console.log ('formdata', formData);
+        const newMovie = {title: req.body.movieTitle, year: parseInt(req.body.movieYear)};
+        frenchMovies = [...frenchMovies,newMovie];
+        res.sendStatus(201);
+    }
 })
 
 /* app.get('/movies-details',function(req,res)
